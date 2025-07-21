e4f792567971e99e2044ac902c59b0cd6d22c4ef
Kamio Hiroshi <nagoya.nomad@gmail.com>
2025-07-21 16:27:52 +0900

    プロジェクトのAPIを実装した。
    テキスト本 pp.207-208のコード

--- api.py
+++ api.py
@@ -1,14 +1,53 @@
 ## 6.3(3) pp.202-205 Flaskを用いたAPIの実装
 
 # ライブラリのインポート
+import pandas as pd
+
 from flask import Flask
 from flask import request
+from flask import make_response
 
 from problem import CarGroupProblem
 
 
 # Flaskのアプリケーション（インスタンス）を作成する。
 app = Flask(__name__)
+
+def preprocess(request):
+    '''
+    リクエストデータを受け取り、データフレームに変換する函数
+    parameter:
+        flask.request:
+    return:
+        (pandas.DataFrame): 生徒のデータフレームと車のデータフレーム
+    '''
+    # 各ファイルを取得する。
+    students = request.files['students']
+    cars = request.files['cars']
+
+    # pandasで読み込む。
+    students_df = pd.read_csv(students)
+    cars_df = pd.read_csv(cars)
+
+    return students_df, cars_df
+
+def postprocess(solution_df):
+    '''
+    データフレームをcsvに変換する函数
+    parameter:
+        solution_df (pandas.DataFrame): データフレーム形式の最適化結果
+    return:
+        (Flask.responce): クライアント側に返すレスポンスデータ(csvファイル)
+    '''
+    solution_csv = solution_df.to_csv(index = False)
+
+    response = make_response()
+
+    response.data = solution_csv
+    response.headers['Content-Type'] = 'text/csv'
+
+    return response
+
 
 # 最適化問題を解くAPI用の函数
 @app.route('/api', methods = ['POST'])
